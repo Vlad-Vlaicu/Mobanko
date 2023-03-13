e0d596380c76d9a8ebd746d9d20e9898e5a9564b
Vlad <vlad_gabriel.vlaicu@stud.acs.upb.ro>
2023-03-13 12:16:56 +0200

    added register button to login page

--- app/src/main/java/com/example/mobanko/activities/LoginActivity.java
+++ app/src/main/java/com/example/mobanko/activities/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.mobanko.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
