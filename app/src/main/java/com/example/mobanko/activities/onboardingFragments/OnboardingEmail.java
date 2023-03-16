3b5317205c78b6f2b44a20ecae4c7281803bb408
vladv <vlad_gabriel.vlaicu@stud.acs.upb.ro>
2023-03-16 15:20:35 +0200

    added email onboarding partial impl

--- app/src/main/java/com/example/mobanko/activities/onboardingFragments/OnboardingEmail.java
+++ app/src/main/java/com/example/mobanko/activities/onboardingFragments/OnboardingEmail.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.mobanko.R;
@@ -39,7 +42,29 @@ public class OnboardingEmail extends Fragment {
 
         titleGreeting.setText(name + ", ce adresa de e-mail ai?");
 
+        var emailEditText = (EditText) view.findViewById(R.id.editTextEmailAddressOnboarding);
+        var emailRetypeEditText = (EditText) view.findViewById(R.id.editTextEmailAddressOnboardingRetype);
+        var firstWarning = (TextView) view.findViewById(R.id.warningTextViewEmailOnboarding);
+        var secondWarning = (TextView) view.findViewById(R.id.warningTextViewEmailRetypeOnboarding);
+        var continueButton = (TextView) view.findViewById(R.id.continueEmailOnboardingButton);
+        var disabledButton = (TextView) view.findViewById(R.id.continueEmailOnboardingButtonDisabled);
 
+        emailEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
         return view;
     }
