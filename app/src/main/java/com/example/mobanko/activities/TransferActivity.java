dc36c70d235a3863f1b4af503a7dbb4cc4276484
Vlad <vlad_gabriel.vlaicu@stud.acs.upb.ro>
2023-05-08 20:27:07 +0300

    added Transfer Type Activity

--- app/src/main/java/com/example/mobanko/activities/TransferActivity.java
+++ app/src/main/java/com/example/mobanko/activities/TransferActivity.java
@@ -1,10 +1,19 @@
 package com.example.mobanko.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mobanko.R;
 import com.example.mobanko.databinding.ActivityTransferBinding;
@@ -14,6 +23,7 @@ public class TransferActivity extends AppCompatActivity {
 
     private ActivityTransferBinding binding;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +36,73 @@ public class TransferActivity extends AppCompatActivity {
         User userInfo = (User) intent.getSerializableExtra("userInfo");
         int accountId = intent.getIntExtra("accountId", -1);
 
-        TextView textView = binding.getRoot().findViewById(R.id.textView21);
-        textView.setText("Account no " + accountId + "starts a new transfer");
+        if (accountId == -1) {
+            finish();
+        }
+
+        ConstraintLayout manualTransferZone = binding.getRoot().findViewById(R.id.manualTransferZone);
+        ConstraintLayout scanPayZone = binding.getRoot().findViewById(R.id.scanPayZone);
+        ConstraintLayout templatesZone = binding.getRoot().findViewById(R.id.templatesZone);
+        ConstraintLayout billPaymentsZone = binding.getRoot().findViewById(R.id.billPaymentsZone);
+
+
+        manualTransferZone.setOnTouchListener((v, event) -> {
+            int action = event.getAction();
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
+                    //TODO: manualTransferActivity
+                    Toast.makeText(this, "manual", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return false;
+        });
+
+        scanPayZone.setOnTouchListener((v, event) -> {
+            int action = event.getAction();
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
+                    //TODO: scanPayZoneActivity
+                    Toast.makeText(this, "scan", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return false;
+        });
+
+        templatesZone.setOnTouchListener((v, event) -> {
+            int action = event.getAction();
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
+                    //TODO: templatesZoneActivity
+                    Toast.makeText(this, "template", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return false;
+        });
+
+        billPaymentsZone.setOnTouchListener((v, event) -> {
+            int action = event.getAction();
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
+                    //TODO: billPaymentsZone
+                    Toast.makeText(this, "bill", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return false;
+        });
+
+
+    }
+
+    private void animateBackgroundColorChange(View view, int targetColor) {
+        ValueAnimator colorAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), ((ColorDrawable) view.getBackground()).getColor(), targetColor);
+        colorAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                int color = (int) animator.getAnimatedValue();
+                view.setBackgroundColor(color);
+            }
+        });
+        colorAnimator.setDuration(300);
+        colorAnimator.start();
     }
 }
