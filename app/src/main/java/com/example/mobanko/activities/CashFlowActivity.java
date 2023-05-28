f5038295da62c5a57e07afc52e4cc32f40463132
Vlad <vlad_gabriel.vlaicu@stud.acs.upb.ro>
2023-05-28 20:47:13 +0300

    initial cashflow layout

---
+++ app/src/main/java/com/example/mobanko/activities/CashFlowActivity.java
@@ -0,0 +1,16 @@
+package com.example.mobanko.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mobanko.R;
+
+public class CashFlowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cash_flow);
+    }
+}
